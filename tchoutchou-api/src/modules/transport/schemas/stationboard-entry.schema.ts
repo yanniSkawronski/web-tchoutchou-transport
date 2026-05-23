24ec39b086e71e3d6f77b140a346649fbe32626c
Aym :) <github.perfected536@passinbox.com>
2026-05-23 22:01:21 +0200

    fix case when station is null because of no query

--- tchoutchou-api/src/modules/transport/schemas/stationboard-entry.schema.ts
+++ tchoutchou-api/src/modules/transport/schemas/stationboard-entry.schema.ts
@@ -4,9 +4,9 @@ import {CheckpointSchema} from './checkpoint.schema.js';
 
 export const StationboardEntrySchema = z.object({
     stop: CheckpointSchema,
-    name: z.string(),
+    name: z.string().nullable(),
     category: z.string(),
-    number: z.string(),
+    number: z.string().nullable(),
     operator: z.string().nullable(),
     to: z.string(),
 });
