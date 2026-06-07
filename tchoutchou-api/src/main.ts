db91188195870a7c8eea3e287740822779350cd4
Aym :) <github.perfected536@passinbox.com>
2026-06-07 18:12:23 +0200

    fix: broken frontend because of no cors.

--- tchoutchou-api/src/main.ts
+++ tchoutchou-api/src/main.ts
@@ -7,6 +7,12 @@ import { AppModule } from './app.module.js';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:4000'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe);
   await app.listen(process.env.PORT ?? 3000);
 }
