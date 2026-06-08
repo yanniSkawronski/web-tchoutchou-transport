a2554193fe9f505c17eb5d4a06148c281d562a07
Skinay <github@skinay.ch>
2026-06-08 15:57:02 +0200

    Better navigation bar

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, MatTabsModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
